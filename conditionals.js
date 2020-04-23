175c56b1ae9631d8a608a962d615852d4f824c7d
Keith Millar <keithmillar@hotmail.ca>
2020-04-23 18:12:47 +0000

    adding temperature practice file

--- conditionals.js
+++ conditionals.js
@@ -1,15 +1,37 @@
-const temperature = 12;
+const temperature = 55;
+let severeTemp = false
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+if (temperature < -40 || temperature > 40) 
+{
+  console.log("Maybe going outside isn't such a great idea…");
+  severeTemp = true
 }
 
-console.log("Now you're ready to go outside!");
+if ((severeTemp == false))
+{
+  if (temperature < 0)
+  {
+    console.log("Make sure you pick out a scarf!");
+  } 
+  else if (temperature < 15) 
+  {
+    console.log("Short sleeves won't cut it!");
+  } 
+  else 
+  {
+    console.log("Short sleeves are fine.");
+  }
+}
 
+//console.log("Now you're ready to go outside!");
+
+
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// }
 
 // const raining = true;
 // const cold = false;
